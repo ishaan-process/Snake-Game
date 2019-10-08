0191c0bf644d6a4c2de1bbf204f5f17229a9d75f
Ishaan Srivastav <ishaan2014@gmail.com>
2019-10-08 16:31:41 +0530

    moving

--- sketch.js
+++ sketch.js
@@ -1,25 +1,51 @@
 let snake;
+let reso=10;
+let food;
+let w;
+let h;
 
 function setup(){
 	createCanvas(400,400);
+	w=floor(width/reso);
+	h=floor(height/reso);
+
+	frameRate(10);
 	snake=new Snake();
+	foodLocation();
+}
+
+function foodLocation(){
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food=createVector(x,y);
+
 }
 function keyPressed(){
-	if(keyCode === UP_ARROW{
+	if(keyCode === UP_ARROW){
 		snake.setDir(0,-1);
 	}
-	else if(keyCode === DOWN_ARROW{
+	else if(keyCode === DOWN_ARROW){
 		snake.setDir(0,1);
 	}
-	else if(keyCode === RIGHT_ARROW{
+	else if(keyCode === RIGHT_ARROW){
 		snake.setDir(1,0);
 	}
-	else if(keyCode === LEFT_ARROW{
+	else if(keyCode === LEFT_ARROW){
 		snake.setDir(-1,0);
 	}
 }
 function draw(){
+	scale(reso);
 	background(220);
+	if(snake.eat(food)){
+	foodLocation();
+	}
 	snake.update();
 	snake.show();
+
+
+	noStroke();
+	fill(255,0,0);
+	rect(food.x,food.y,1,1);
+
 }
--- snake.js
+++ snake.js
@@ -5,13 +5,28 @@ class Snake{
 		this.xdir = 1;
 		this.ydir = 0;
 	}
+	setDir(x,y){
+		this.xdir = x;
+		this.ydir = y;
+	}
 	update(){
 		this.body[0].x += this.xdir;
 		this.body[0].y += this.ydir;
 
 	}
+	eat(pos){
+		let x = this.body[0].x;
+		let y = this.body[0].y;
+		if(x===pos.x && y===pos.y){
+			print("FOOD EATEN");
+			return true;
+		}	
+		return false;	
+	}
+
 	show(){
 		fill(0);
-		rect(this.body[0].x, this.body[0].y,10,10);
+		noStroke();
+		rect(this.body[0].x, this.body[0].y, 1, 1);
 	}
 }
