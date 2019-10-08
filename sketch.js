11b8da3f8b5db70107cb7e2d5dc9e302c43e60d1
Ishaan Srivastav <ishaan2014@gmail.com>
2019-10-08 16:10:45 +0530

    first

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let snake;
+
+function setup(){
+	createCanvas(400,400);
+	snake=new Snake();
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW{
+		snake.setDir(0,-1);
+	}
+	else if(keyCode === DOWN_ARROW{
+		snake.setDir(0,1);
+	}
+	else if(keyCode === RIGHT_ARROW{
+		snake.setDir(1,0);
+	}
+	else if(keyCode === LEFT_ARROW{
+		snake.setDir(-1,0);
+	}
+}
+function draw(){
+	background(220);
+	snake.update();
+	snake.show();
+}
